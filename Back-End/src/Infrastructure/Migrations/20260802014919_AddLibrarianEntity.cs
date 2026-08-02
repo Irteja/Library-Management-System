f57576b7114d6c3fea6f6bbd252baa43bd240c94
Irteja <irteja500@gmail.com>
2026-08-02 19:55:40 +0600

    move the migration into the correct folder

--- Back-End/src/Infrastructure/Migrations/20260802014919_AddLibrarianEntity.cs
+++ Back-End/src/Infrastructure/Migrations/20260802014919_AddLibrarianEntity.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Infrastructure.Data.Migrations
+namespace Infrastructure.Migrations
 {
     /// <inheritdoc />
     public partial class AddLibrarianEntity : Migration
